ef4f0755089df55a3102761cff233e05bf62d520
Jesse Millar <hellojessemillar@gmail.com>
2015-05-04 16:54:33 -0600

    Linting

--- src/anitrace.js
+++ src/anitrace.js
@@ -1,27 +1,26 @@
-var AniTrace = function()
-{
-	this.trace = function(div, delay, duration, easing)
-	{
-		document.getElementById(div).style.visibility = 'visible' // Fix a Safari bug (I hate that browser)
+var AniTrace = function() {
+    this.trace = function(div, delay, duration, easing) {
+        document.getElementById(div).style.visibility = 'visible'; // Fix a Safari bug (I hate that browser)
 
-		var paths = document.getElementById(div).getElementsByTagNameNS('http://www.w3.org/2000/svg', 'path') // Gather all the paths
+        var paths = document.getElementById(div).getElementsByTagNameNS('http://www.w3.org/2000/svg', 'path'); // Gather all the paths
 
-		for (var i = 0; i < paths.length; i++) // Play with the paths so they draw
-	    {
-	        var path = paths[i]
-	        var length = path.getTotalLength()
+        for (var i = 0; i < paths.length; i++) // Play with the paths so they draw
+        {
+            var path = paths[i];
+            var length = path.getTotalLength();
 
-	        path.style.strokeDasharray = length + ' ' + length
-	        path.style.strokeDashoffset = length
+            path.style.strokeDasharray = length + ' ' + length;
+            path.style.strokeDashoffset = length;
 
-	        this.draw(path, delay, duration, easing)
-	    }
-	}
+            this.draw(path, delay, duration, easing);
+        }
+    };
 
-	this.draw = function(path, delay, duration, easing) // Needed closure
-	{
-		setTimeout(function() {
-			$(path).animate({strokeDashoffset: 0}, duration, easing)
-		}, delay)
-	}
-}
+    this.draw = function(path, delay, duration, easing) { // Needed closure
+        setTimeout(function() {
+            $(path).animate({
+                strokeDashoffset: 0
+            }, duration, easing);
+        }, delay);
+    };
+};
